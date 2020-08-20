a287cd5a1fa9e88a74ac9edfe5a8e0db99bdf194
shivakant789 <62831429+shivakant789@users.noreply.github.com>
2020-08-21 01:42:01 +0530

    Find you available agents

--- config.php
+++ config.php
@@ -3,6 +3,7 @@
 //remote database connection 
 $conn = mysqli_connect("remotemysql.com"," 65J2cJtdpu","QNf4fmLUZ1","65J2cJtdpu");
 if(!$conn){
+    echo "not successfull";
 	die("Connection error: " . mysqli_connect_error());	
 }
 ?>
